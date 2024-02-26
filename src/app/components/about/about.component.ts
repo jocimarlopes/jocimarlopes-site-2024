3d53275cd7834c276d32725db01f9b0e8c5f1075
Jocimar Lopes <usuario@MacBook-Air-de-Jocimar.local>
2024-02-25 21:22:14 -0300

    feature/animations_about

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import gsap, { Power1 } from "gsap";
+import SplitType from 'split-type'
 
 @Component({
   selector: 'about',
@@ -8,11 +10,22 @@ import { Component, OnInit } from '@angular/core';
 export class AboutComponent  implements OnInit {
 
   icons: any = []
+  q: any
 
-  constructor() { }
+  constructor(
+    private el: ElementRef
+  ) {
+    this.q = gsap.utils.selector(el);
+
+  }
 
   ngOnInit() {
     this.addIcons()
+    this.initAnimations()
+  }
+
+  initAnimations() {
+    this.animateHeaderButtons()
   }
 
   addIcons() {
@@ -23,6 +36,22 @@ export class AboutComponent  implements OnInit {
       'logo-google',
       'logo-amazon'
     ]
+  }
+
+  async animateHeaderButtons() {
+    setTimeout(async () => {
+      await gsap.from(this.q(".buttons"),
+        {
+          delay: 2,
+          y: '-15vh',
+          opacity: 1,
+          stagger: 0.4,
+          duration: 0.8,
+          ease: 'bounce.out',
+          repeat: 0
+        }
+      )
+    }, 20);
   }
 
 }
