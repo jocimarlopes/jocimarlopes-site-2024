f106eac00c8293c1998d9568cfeaa94ab9d3d597
Jocimar Lopes <lopes.jocimar@gmail.com>
2025-04-14 02:13:56 -0300

    bugfix/added_language_pdf_name

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -113,7 +113,7 @@ export class HomePage implements OnInit {
       }
     }
 
-    pdf.save('Jocimar Lopes - Full Stack Developer.pdf');
+    pdf.save(`Jocimar Lopes - Full Stack Developer (${this.selectedLang}).pdf`);
     div.classList.remove('pdf-content');
     div.classList.remove('force-desktop');
   });
